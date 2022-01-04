9c132f1d544e62cf09e53e497a2cb40c0fcf788e
Vitorio Venturi <v.venturi@yahoo.com.br>
2022-01-04 16:04:07 -0300

    Commit inicial

---
+++ src/main/java/com/vventuri/desafiopubfuture/DesafioPubfutureApplication.java
@@ -0,0 +1,13 @@
+package com.vventuri.desafiopubfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPubfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPubfutureApplication.class, args);
+	}
+
+}
